65153181391371a0f41829374b76948b61a1d373
meetinjp <meetinjp@proton.me>
2024-10-15 20:30:54 +0200

    feat: Zadanie 8, Zadanie 9

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,1 +1,21 @@
 ﻿Console.WriteLine("Hello WSEI");
+
+// Zadanie 8
+Console.WriteLine("Hello World!");
+Console.Write("##");
+Console.Write("# #");
+Console.Write("##");
+Console.WriteLine();
+Console.WriteLine("Hello World!");
+
+// Zadanie 9
+Console.Write("##");
+Console.Write("##");
+Console.WriteLine("##");
+Console.Write("##");
+Console.Write(" ");
+Console.Write(" ");
+Console.WriteLine("##");
+Console.Write("##");
+Console.Write("##");
+Console.WriteLine("##");
