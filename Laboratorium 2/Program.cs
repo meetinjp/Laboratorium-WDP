253953d44a7f529c2a0f731b6e75768d544fb3f5
meetinjp <meetinjp@proton.me>
2024-10-16 00:51:36 +0200

    feat(Laboratorium_2): Zadanie1

---
+++ Laboratorium 2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Laboratorium_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Zadanie1();
+        }
+
+        public static void Zadanie1()
+        {
+            int number = 5;
+            int cyfraSetek;
+            int cyfraDziesiątek;
+            int cyfraJedności;
+
+            cyfraSetek = number / 100; // "w zmiennej number jest liczba mniejsza od 1000"
+            cyfraDziesiątek = number / 10 % 10;
+            cyfraJedności = number % 10;
+
+            Console.WriteLine($"Cyfra setek: {cyfraSetek}");
+            Console.WriteLine($"Cyfra dziesiątek: {cyfraDziesiątek}");
+            Console.WriteLine($"Cyfra jedności: {cyfraJedności}");
+        }
+    }
+}
