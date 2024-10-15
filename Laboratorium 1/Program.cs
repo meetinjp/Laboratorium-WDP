8835a0c330043815391a41959005641c9c43f301
meetinjp <meetinjp@proton.me>
2024-10-16 01:13:44 +0200

    refactor(Test): move code to Laboratorium 1

--- Laboratorium 1/Program.cs
+++ Laboratorium 1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Zadanie 8
+Console.WriteLine("Hello World!");
+Console.Write("##");
+Console.Write("# #");
+Console.Write("##");
+Console.WriteLine();
+Console.WriteLine("Hello World!");
+
+// Zadanie 9
+Console.Write("##");
+Console.Write("##");
+Console.WriteLine("##");
+Console.Write("##");
+Console.Write(" ");
+Console.Write(" ");
+Console.WriteLine("##");
+Console.Write("##");
+Console.Write("##");
+Console.WriteLine("##");
+
+// Zadanie 10
+// NOTE: Tabs move the cursor to the next tab stop!
+Console.WriteLine("\t123\n+\t235\n\t---\n\t358");
