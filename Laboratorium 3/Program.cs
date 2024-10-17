f8226297f641bc80f308a5be32a963469fe09711
meetinjp <meetinjp@proton.me>
2024-10-17 23:59:37 +0200

    feat: add Laboratorium_3, feat(Laboratorium_3): Zadanie1, Zadanie2

---
+++ Laboratorium 3/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Laboratorium_3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Zadanie1();
+        Zadanie2();
+    }
+
+    public static void Zadanie1()
+    {
+        decimal balans = 56;
+        bool isCreditCardValid = false;
+        decimal price = 45;
+        bool canPay;
+
+        canPay = balans >= price && isCreditCardValid;
+
+        Console.WriteLine(canPay);
+    }
+
+    public static void Zadanie2()
+    {
+        double a = 5;
+        double b = 1;
+        double c = 3;
+        bool isTriangle;
+
+        isTriangle = a + b > c && b + c > a && c + a > b; // twierdzenie o nierówności trójkąta
+
+        Console.WriteLine(isTriangle);
+    }
+}
