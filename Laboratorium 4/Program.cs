57ca8fbaa534465e6aced635d520f7b8bedaa94e
meetinjp <meetinjp@proton.me>
2024-10-19 19:41:46 +0200

    feat: add Laboratorium_4, feat(Laboratorium_4): Zadanie1, Zadanie2

---
+++ Laboratorium 4/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Laboratorium_4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Zadanie1();
+        Zadanie2();
+    }
+
+    static void Zadanie1()
+    {
+        double[] factors = { 2.4, 1.9, -0.5 };
+
+        if (factors.Length < 3)
+        {
+            Console.WriteLine("Nie można obliczyć równania, nie ma wszystkich współczynników.");
+
+            return;
+        }
+
+        if (factors.Length > 3)
+        {
+            Console.WriteLine(
+                "Nie można obliczyć równania, zostało podane zbyt wiele współczynników."
+            );
+
+            return;
+        }
+
+        double x = 2.3;
+        double a = factors[0];
+        double b = factors[1];
+        double c = factors[2];
+
+        Console.WriteLine(a * Math.Pow(x, 2) + b * x + c);
+    }
+
+    static void Zadanie2()
+    {
+        double[] arr = { 12.5, 6.7, 2.89, 10.1, 2.45 };
+
+        for (int i = arr.Length - 1; i >= 0; --i)
+        {
+            Console.WriteLine(arr[i]);
+        }
+    }
+}
