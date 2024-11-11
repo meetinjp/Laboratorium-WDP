7222a140c01f83bdd7a9e57024eb9bbc7683c52b
meetinjp <meetinjp@proton.me>
2024-11-11 13:44:52 +0100

    feat(Laboratorium_5): add QuadraticEquation (Zadanie 4), Zadanie5

--- Laboratorium 5/Program.cs
+++ Laboratorium 5/Program.cs
@@ -1,11 +1,13 @@
-﻿namespace Laboratorium_5;
+﻿using System.Collections.Immutable;
+
+namespace Laboratorium_5;
 
 class Program
 {
     static void Main()
     {
         // Menu.Display();
-        Console.WriteLine(QuadraticEquation(2.0, 1.0, 2.0, 3.0));
+        Console.WriteLine(Zadanie5());
     }
 
     /// <summary>Zadanie 1</summary>
@@ -29,5 +31,43 @@ class Program
     static double QuadraticEquation(double x = 0.0, double a = 1.0, double b = 1.0, double c = 1.0)
     {
         return a * Math.Pow(x, 2) + b * x + c;
+    }
+
+    /// <summary>Zadanie 4</summary>
+    static double QuadraticEquation()
+    {
+        ImmutableArray<char> names = ImmutableArray.Create('x', 'a', 'b', 'c');
+        int i = 0;
+        double[] values = new double[4];
+
+        do
+        {
+            Console.Write($"Podaj wartość {names[i]}: ");
+
+            if (!double.TryParse(Console.ReadLine(), out double arg))
+            {
+                continue;
+            }
+
+            values[i++] = arg;
+        } while (i < names.Length && i < values.Length);
+
+        return QuadraticEquation(values[0], values[1], values[2], values[3]);
+    }
+
+    static decimal Zadanie5()
+    {
+        int code = 2;
+        decimal price = 10;
+        decimal tax =
+            code == 0
+                ? price * 0.08m
+                : (
+                    code == 1 ? price * 0.23m
+                    : code == 2 ? price * 0.17m
+                    : price * 0.12m
+                );
+
+        return tax;
     }
 }
